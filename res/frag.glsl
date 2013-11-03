0cac568aeb90322e353883a1708b6e6b0ed0f1ea
Matt Thompson <matthewbot@gmail.com>
2013-11-03 12:25:05 -0500

    Hacked in slightly better lighting until I decide how lighting will actually work in game.

--- res/frag.glsl
+++ res/frag.glsl
@@ -2,16 +2,24 @@
 
 uniform sampler2DArray sampler;
 
+smooth in vec4 fragpos;
 smooth in vec4 fragnormal;
+smooth in vec4 fraglight1;
+smooth in vec4 fraglight2pos;
 smooth in vec3 fragtex;
 
 out vec4 outputColor;
 
-const vec4 lightdir = normalize(vec4(.2, 1, -.6, 0));
-
 void main() {
     vec4 texel = texture(sampler, fragtex);
-    float light = .7*max(dot(fragnormal, -lightdir), 0) + .3;
-    outputColor.rgb = texel.rgb * light;
+
+    vec4 fraglight2dist = fraglight2pos - fragpos;
+    
+    float intensity = 0;
+    intensity += max(-dot(fragnormal, fraglight1), 0.0);
+    intensity += max(100*dot(fragnormal, normalize(fraglight2dist))
+                     /dot(fraglight2dist, fraglight2dist), 0.0);
+    
+    outputColor.rgb = (texel * clamp(intensity, .2, 1.5)).rgb;
     outputColor.a = texel.a;
 }
--- res/vert.glsl
+++ res/vert.glsl
@@ -7,11 +7,19 @@ layout(location = 2) in vec3 tex;
 uniform mat4 perspective;
 uniform mat4 modelview;
 
+const vec4 lightdir = normalize(vec4(.3, .5, -1, 0));
+
+smooth out vec4 fragpos;
 smooth out vec4 fragnormal;
+smooth out vec4 fraglight1;
+smooth out vec4 fraglight2pos;
 smooth out vec3 fragtex;
 
 void main() {
-    gl_Position = perspective*modelview*vec4(position, 1);
-    fragnormal = normalize(vec4(normal, 0));
+    fragpos = modelview*vec4(position, 1);
+    gl_Position = perspective*fragpos;
+    fragnormal = normalize(modelview*vec4(normal, 0));
+    fraglight1 = normalize(modelview*lightdir);
+    fraglight2pos = vec4(0, 0, 0, 1);
     fragtex = tex;
 }
